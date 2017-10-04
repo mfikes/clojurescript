996ea3c5167db0c93f7a558c9bc6134b314f3b07
Erik Assum <erik@assum.net>
2017-10-04 20:50:33 +0200

    CLJS-2268 Make clojure.string/escape constent with Clojure
    - treat `cmap` as a function, not as a map

--- src/main/cljs/clojure/string.cljs
+++ src/main/cljs/clojure/string.cljs
@@ -239,7 +239,7 @@
       (if (== length index)
         (. buffer (toString))
         (let [ch (.charAt s index)
-              replacement (get cmap ch)]
+              replacement (cmap ch)]
           (if-not (nil? replacement)
             (.append buffer (str replacement))
             (.append buffer ch))
