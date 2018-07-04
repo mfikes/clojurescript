a06e0045ad04dad9752a220aa7776e44319857b5
Mike Fikes <mike@fikesfarm.com>
2018-07-03 20:50:50 -0400

    CLJS-2802: empty? is broken for transient collections

--- src/test/cljs/cljs/collections_test.cljs
+++ src/test/cljs/cljs/collections_test.cljs
@@ -1027,6 +1027,21 @@
               (chunk-append b 0)
               (next (chunk-cons (chunk b) nil))))))
 
+(defn test-empty?-on-transient [coll]
+  {:pre [(empty? coll)]}
+  (let [tcoll (transient coll)
+        _     (is (empty? tcoll))
+        tcoll (conj! tcoll [1 2])
+        _     (is (not (empty? tcoll)))
+        _     (persistent! tcoll)
+        _     (is (thrown? js/Error (empty? tcoll)))]))
+
+(deftest test-cljs-2802
+  (test-empty?-on-transient [])
+  (test-empty?-on-transient #{})
+  (test-empty?-on-transient (array-map))
+  (test-empty?-on-transient (hash-map)))
+
 (comment
 
   (run-tests)
