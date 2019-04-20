4ad5d56efa5d2a163e8cceb0e9e05eb97c905d86
Erik Assum <erik@assum.net>
2019-04-20 21:23:31 +0200

    CLJS-1908 Wrap vars in throw message with `pr-str`

--- src/main/cljs/cljs/source_map/base64.cljs
+++ src/main/cljs/cljs/source_map/base64.cljs
@@ -16,10 +16,10 @@
   (let [e (find int->char n)]
    (if e
      (second e)
-     (throw (js/Error. (str "Must be between 0 and 63: " n))))))
+     (throw (js/Error. (str "Must be between 0 and 63: " (pr-str n)))))))
 
 (defn decode [c]
   (let [e (find char->int c)]
    (if e
      (second e)
-     (throw (js/Error. (str "Not a valid base 64 digit: " c))))))
+     (throw (js/Error. (str "Not a valid base 64 digit: " (pr-str c)))))))
