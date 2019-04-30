43037bee62977bf67f49dfe9a6edec32766e61ac
lread <lee@dlread.com>
2019-04-30 16:47:19 -0400

    CLJS-3074 Resolve :warning-handlers compiler option when symbols
    Compiler option :warning-handlers are now resolved from symbol to var making
    them usable when invoked via cljs.main.
    
    See JIRA ticket for details.

--- src/test/clojure/cljs/analyzer_api_tests.clj
+++ src/test/clojure/cljs/analyzer_api_tests.clj
@@ -7,8 +7,10 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns cljs.analyzer-api-tests
-  (:require [cljs.analyzer.api :as ana-api])
-  (:use clojure.test))
+  (:require [cljs.analyzer.api :as ana-api]
+            [clojure.string :as string])
+  (:use clojure.test)
+  (:import clojure.lang.ExceptionInfo))
 
 (deftest cljs-warning-test
   (is (ana-api/warning-enabled? :undeclared-var)
@@ -40,3 +42,36 @@
       (ana-api/analyze test-cenv test-env warning-form nil
                        {:warning-handlers [(warning-handler counter)]}))
     (is (= 1 @counter))))
+
+(defn resolve-handler1 [warning-type env extra]
+  (println "H1" warning-type))
+
+(defn resolve-handler2 [warning-type env extra]
+  (println "H2" warning-type))
+
+(deftest with-resolve-warning-handlers-test
+  (is (= ["H1 :fn-arity" "H2 :fn-arity"]
+         (string/split-lines
+          (with-out-str
+            (ana-api/analyze
+             test-cenv test-env warning-form nil
+             (clojure.edn/read-string
+              "{:warning-handlers [cljs.analyzer-api-tests/resolve-handler1
+                                  cljs.analyzer-api-tests/resolve-handler2]}")))))))
+
+(deftest with-resolve-warning-handlers-misconfigure-test
+  (is (thrown-with-msg? ExceptionInfo #":warning-handlers symbol unqualified-sym is not fully qualified"
+                       (ana-api/analyze
+                        test-cenv test-env warning-form nil
+                        (clojure.edn/read-string
+                         "{:warning-handlers [unqualified-sym]}"))))
+  (is (thrown-with-msg? ExceptionInfo #"Cannot require namespace referred by :warning-handlers value wont-find-this-ns/sym"
+                        (ana-api/analyze
+                         test-cenv test-env warning-form nil
+                         (clojure.edn/read-string
+                          "{:warning-handlers [wont-find-this-ns/sym]}"))))
+  (is (thrown-with-msg? ExceptionInfo #":warning-handlers symbol cljs.analyzer-api-tests/unfound-sym not found"
+                        (ana-api/analyze
+                         test-cenv test-env warning-form nil
+                         (clojure.edn/read-string
+                          "{:warning-handlers [cljs.analyzer-api-tests/unfound-sym]}")))))
