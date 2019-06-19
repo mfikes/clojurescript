a27971437a62470f6c50caa13cf529fa7945aa1d
Mike Fikes <mike@fikesfarm.com>
2019-06-19 19:07:16 -0400

    CLJS-3115: Restrict -conj on maps via seqs to map entries

--- src/main/cljs/cljs/core.cljs
+++ src/main/cljs/cljs/core.cljs
@@ -6842,7 +6842,7 @@ reduces them without incurring seq initialization"
         (if (nil? es)
           ret
           (let [e (first es)]
-            (if (vector? e)
+            (if (map-entry? e)
               (recur (-assoc ret (-nth e 0) (-nth e 1))
                      (next es))
               (throw (js/Error. "conj on a map takes map entries or seqables of map entries"))))))))
@@ -7892,7 +7892,7 @@ reduces them without incurring seq initialization"
         (if (nil? es)
           ret
           (let [e (first es)]
-            (if (vector? e)
+            (if (map-entry? e)
               (recur (-assoc ret (-nth e 0) (-nth e 1))
                      (next es))
               (throw (js/Error. "conj on a map takes map entries or seqables of map entries"))))))))
@@ -8765,7 +8765,7 @@ reduces them without incurring seq initialization"
         (if (nil? es)
           ret
           (let [e (first es)]
-            (if (vector? e)
+            (if (map-entry? e)
               (recur (-assoc ret (-nth e 0) (-nth e 1))
                      (next es))
               (throw (js/Error. "conj on a map takes map entries or seqables of map entries"))))))))
