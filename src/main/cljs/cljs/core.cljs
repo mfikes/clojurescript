fadff7331ad29ff447be1c266d6fc99f4abac86a
Mike Fikes <mike@fikesfarm.com>
2019-06-19 22:06:21 -0400

    CLJS-3118: Resurrect -with-meta optimizations

--- src/main/cljs/cljs/core.cljs
+++ src/main/cljs/cljs/core.cljs
@@ -5722,7 +5722,7 @@ reduces them without incurring seq initialization"
     [(key coll) (val coll)]
 
     (vector? coll)
-    (with-meta coll nil)
+    (-with-meta coll nil)
 
     (array? coll)
     (.fromArray PersistentVector coll true)
@@ -6669,7 +6669,7 @@ reduces them without incurring seq initialization"
 
   IWithMeta
   (-with-meta [node meta]
-    (with-meta [key val] meta))
+    (-with-meta [key val] meta))
 
   IStack
   (-peek [node] val)
