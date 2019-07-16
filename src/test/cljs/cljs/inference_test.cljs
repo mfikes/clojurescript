7bd9b16b9413d18bbc853dcb71c6f663252d2416
roman01la <roman01la@romanliutikov.com>
2019-07-16 15:18:04 +0300

    CLJS-3106: Remove return type hint from distinct?

--- src/test/cljs/cljs/inference_test.cljs
+++ src/test/cljs/cljs/inference_test.cljs
@@ -77,7 +77,8 @@
     (delay? nil)
     (uuid? nil)
     (tagged-literal? nil)
-    (cljs.pprint/float? nil)))
+    (cljs.pprint/float? nil)
+    (distinct? nil)))
 
 (deftest cljs-2866-test
   ;; Here we are testing that in the JavaScript emitted,
