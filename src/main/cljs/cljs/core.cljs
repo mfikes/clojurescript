800fab0bcda83ca8528d66b7fbefbcbad91828c0
Michiel Borkent <michielborkent@gmail.com>
2019-07-18 10:31:14 +0200

    CLJS-3136: mark deprecated vars as such using metadata

--- src/main/cljs/cljs/core.cljs
+++ src/main/cljs/cljs/core.cljs
@@ -2895,6 +2895,7 @@ reduces them without incurring seq initialization"
 
 (defn bit-shift-right-zero-fill
   "DEPRECATED: Bitwise shift right with zero fill"
+  {:deprecated "1.7.10"}
   [x n] (cljs.core/bit-shift-right-zero-fill x n))
 
 (defn unsigned-bit-shift-right
@@ -4973,6 +4974,7 @@ reduces them without incurring seq initialization"
 (defn replicate
   "DEPRECATED: Use 'repeat' instead.
   Returns a lazy seq of n xs."
+  {:deprecated "1.8.51"}
   [n x] (take n (repeat x)))
 
 (defn repeatedly
